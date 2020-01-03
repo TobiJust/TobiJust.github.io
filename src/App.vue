fe84538669cd0396509ca5892999bdb408fada38
tobijust <just.tobi@web.de>
2020-01-03 22:58:56 +0100

    Add basic functionality
    This commit adds the vue-router, a navbar and a test tools for starting
    the project.

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+body {
+  margin: 0;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 0;
+  margin: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  float: left;
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+#nav a:hover:not(.active) {
+  color: #42b983;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="top-bar">
+      <ul>
+          <li>Home</li>
+          <li>Fleet</li>
+          <li>Contact</li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.top-bar {
+
+}
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover:not(.active) {
+  background-color: #111;
+}
+
+.active {
+  background-color: #4CAF50;
+}
+</style>
