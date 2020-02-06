9a7b2e37e69225a5f77fedb1bbd7057141fe4af4
tobijust <just.tobi@web.de>
2020-02-06 21:22:19 +0100

    Add vuetify

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/tobijust.github.io/" : "/"
-};
+  "publicPath": "/",
+  "transpileDependencies": [
+    "vuetify"
+  ]
+}
